81f9d7776c8ec40a09374bcb83c4a169641b9c40
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-12 22:43:49 +0530

    Show page and create page has been added

--- client/src/pages/CreateBook.jsx
+++ client/src/pages/CreateBook.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Backbutton from "../components/Backbutton";
 
 function CreateBook() {
-  return (
-    <div>CreateBook</div>
-  )
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [year, setYear] = useState("");
+	const handleTitle = (e) => {
+		setTitle(e.target.value);
+	};
+	const handleAuthor = (e) => {
+		setAuthor(e.target.value);
+	};
+	const handleYear = (e) => {
+		setYear(e.target.value);
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post("http://localhost:3000/books", {
+				title,
+				author,
+				year,
+			});
+			console.log("Data submitted");
+		} catch (error) {
+			console.log(error);
+		}
+		console.log(title, author, year);
+	};
+
+	return (
+		<div className=" flex items-center h-screen  bg-gray-400">
+			<div className="">
+				<Backbutton />
+				CreateBook
+				<form onSubmit={handleSubmit}>
+					<div className="border-2 border-solid border-blue-600 bg-blue-100 rounded-md  ml-10 h-40 p-4 flex items-center">
+						<div className="space-y-2">
+							<div className=" flex">
+								<span className="w-24">Title</span>
+								<span>
+									<input
+										className=" bg-slate-400"
+										type="textbox"
+										onChange={handleTitle}
+										value={title}
+									></input>
+								</span>
+							</div>
+							<div className="flex">
+								<span className="w-24">Author</span>
+								<span>
+									<input
+										className=" bg-slate-400"
+										type="textbox"
+										onChange={handleAuthor}
+										value={author}
+									></input>
+								</span>
+							</div>
+							<div className="flex">
+								<span className="w-24">Year</span>
+								<span>
+									<input
+										className=" bg-slate-400"
+										type="number"
+										onChange={handleYear}
+										value={year}
+									></input>
+								</span>
+							</div>
+							<div></div>
+							<button type="submit" className=" bg-red-100">
+								Submit
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
-export default CreateBook
+export default CreateBook;
--- client/src/pages/ShowBook.jsx
+++ client/src/pages/ShowBook.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import Backbutton from "../components/Backbutton";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../components/Spinner";
-
+import Backbutton from "../components/Backbutton";
 function ShowBook() {
 	const { id } = useParams();
 	const [book, setBook] = useState({});
@@ -23,19 +21,44 @@ function ShowBook() {
 		};
 
 		fetchData();
-	}, []);
+	}, [id]);
 	// console.log(book);
 	return (
-		<div>
+		<div className=" bg-gray-300 h-screen">
 			<Backbutton />
 			{loading ? (
 				<Spinner />
 			) : (
-				<div>
-					<h1>{book.title}</h1>
-					<h2>{book.author}</h2>
-					<h3>{book.year}</h3>
-					<h3>{book._id}</h3>
+				<div className="border-2 border-solid border-blue-600 bg-blue-100 rounded-md w-1/3 ml-10 h-40 p-4 flex items-center">
+					<div className="space-y-2">
+						<div className="flex">
+							<span className="text-lg font-semibold text-blue-700 w-24">
+								Title:
+							</span>
+							<span className="text-lg text-gray-900">{book.title}</span>
+						</div>
+
+						<div className="flex">
+							<span className="text-lg font-semibold text-blue-700 w-24">
+								Author:
+							</span>
+							<span className="text-lg text-gray-900">{book.author}</span>
+						</div>
+
+						<div className="flex">
+							<span className="text-lg font-semibold text-blue-700 w-24">
+								Year:
+							</span>
+							<span className="text-lg text-gray-900">{book.year}</span>
+						</div>
+
+						<div className="flex">
+							<span className="text-lg font-semibold text-blue-700 w-24">
+								ID:
+							</span>
+							<span className="text-lg text-gray-900">{book._id}</span>
+						</div>
+					</div>
 				</div>
 			)}
 		</div>
