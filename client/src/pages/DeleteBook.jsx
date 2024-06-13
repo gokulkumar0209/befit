934a41313a6da40968f8dbf4ac79f4caaf6048bd
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-13 22:08:14 +0530

    CRUD operations are done and application is working fine

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Backbutton from "../components/Backbutton";
 
 function DeleteBook() {
-  return (
-    <div>DeleteBook</div>
-  )
+	const [book, setBook] = useState({});
+	const { id } = useParams();
+	const navigate = useNavigate();
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const result = await axios.get(`http://localhost:3000/books/${id}`);
+				setBook(result.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, [id]);
+	const handleDelete = async () => {
+		try {
+			const deleteBook = await axios.delete(
+				`http://localhost:3000/books/${id}`
+			);
+			console.log("deleted");
+			navigate("/");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div>
+			<Backbutton />
+			DeleteBook
+			<div className="border-2 border-solid border-blue-600 bg-blue-100 rounded-md w-1/3 ml-10 h-40 p-4 flex items-center">
+				<div className="space-y-2">
+					<div className="flex">
+						<span className="text-lg font-semibold text-blue-700 w-24">
+							Title:
+						</span>
+						<span className="text-lg text-gray-900">{book.title}</span>
+					</div>
+
+					<div className="flex">
+						<span className="text-lg font-semibold text-blue-700 w-24">
+							Author:
+						</span>
+						<span className="text-lg text-gray-900">{book.author}</span>
+					</div>
+
+					<div className="flex">
+						<span className="text-lg font-semibold text-blue-700 w-24">
+							Year:
+						</span>
+						<span className="text-lg text-gray-900">{book.year}</span>
+					</div>
+
+					<div className="flex">
+						<span className="text-lg font-semibold text-blue-700 w-24">
+							ID:
+						</span>
+						<span className="text-lg text-gray-900">{book._id}</span>
+					</div>
+				</div>
+			</div>
+			<button onClick={handleDelete} className=" bg-red-400 m-4 ml-9">
+				Delete
+			</button>
+		</div>
+	);
 }
 
-export default DeleteBook
+export default DeleteBook;
--- client/src/pages/EditBook.jsx
+++ client/src/pages/EditBook.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Backbutton from "../components/Backbutton";
 
 function EditBook() {
-  return (
-    <div>EditBook</div>
-  )
+	const { id } = useParams();
+	const [book, setBook] = useState({ title: "", author: "", year: "" });
+	// const [book, setBook] = useState("");
+	// const [title, setTitle] = useState("");
+	// const [author, setAuthor] = useState("");
+	// const [year, setYear] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	useEffect(() => {
+		let isMounted = true;
+		const fetchdata = async () => {
+			setLoading(true);
+			try {
+				const result = await axios.get(`http://localhost:3000/books/${id}`);
+				if (isMounted) {
+					setBook(result.data);
+				}
+				// setBook(result.data);
+				// setTitle(result.data.title);
+				// setAuthor(result.data.author);
+				// setYear(result.data.year);
+			} catch (error) {
+				setError(error);
+			} finally {
+				if (isMounted) {
+					setLoading(false);
+				}
+			}
+		};
+		fetchdata();
+		return () => {
+			isMounted = false;
+		};
+	}, [id]);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setBook((book) => ({
+			...book,
+			[name]: value,
+		}));
+	};
+	// const handleTitle = (e) => {
+	// 	setTitle(e.target.value);
+	// };
+	// const handleAuthor = (e) => {
+	// 	setAuthor(e.target.value);
+	// };
+	// const handleYear = (e) => {
+	// 	setYear(e.target.value);
+	// };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const result = await axios.put(`http://localhost:3000/books/${id}`, book);
+			console.log("Updated");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div>
+			<Backbutton />
+			EditBook
+			<form onSubmit={handleSubmit}>
+				<input
+					type="textbox"
+					className=" bg-blue-200"
+					name="title"
+					value={book.title}
+					onChange={handleChange}
+				></input>
+				<input
+					type="textbox"
+					className=" bg-blue-200"
+					name="author"
+					value={book.author}
+					onChange={handleChange}
+				></input>
+				<input
+					type="Number"
+					className=" bg-blue-200"
+					name="year"
+					value={book.year}
+					onChange={handleChange}
+				></input>
+				<button type="submit" className="bg-red-300">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
-export default EditBook
+export default EditBook;
