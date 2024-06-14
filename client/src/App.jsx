3fd4f6e7205afe5fef9ab23ffb4a6ec6a75f93cb
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-14 16:20:06 +0530

    Navbar added in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CreateBook from "./pages/CreateBook";
 import DeleteBook from "./pages/DeleteBook";
 import EditBook from "./pages/EditBook";
 import Home from "./pages/Home";
 import ShowBook from "./pages/ShowBook";
+import NavBar from "./components/NavBar";
 function App() {
 	return (
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/books/create" element={<CreateBook/>}></Route>
-      <Route path="/books/delete/:id" element={<DeleteBook/>}></Route>
-      <Route path="/books/details/:id" element={<ShowBook/>}></Route>
-      <Route path="/books/edit/:id" element={<EditBook/>}></Route>
-    </Routes>
-  );
+		<div>
+			<div>
+				<NavBar />
+			</div>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/books/create" element={<CreateBook />}></Route>
+				<Route path="/books/delete/:id" element={<DeleteBook />}></Route>
+				<Route path="/books/details/:id" element={<ShowBook />}></Route>
+				<Route path="/books/edit/:id" element={<EditBook />}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
